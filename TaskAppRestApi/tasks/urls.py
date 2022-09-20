312689f31defc8ac847b25356e475278174c9af3
LogKul <89537266+LogKul@users.noreply.github.com>
2022-09-20 11:08:00 -0600

    Added testing Django REST API w/ MongoDD
    The API is fully functional, but only with a very simple dummy dataset as a "proof-of-concept" of utilizing Django's ORM with MongoDB.

---
+++ TaskAppRestApi/tasks/urls.py
@@ -0,0 +1,11 @@
+from django.urls import re_path
+from tasks import views
+
+urlpatterns = [
+    re_path(r'^authors$', views.author),
+    re_path(r'^authors/(?P<pk>[0-9]+)$', views.single_author),
+    re_path(r'^books$', views.book),
+    re_path(r'^authorbooks$', views.author_books),
+    #re_path(r'^/tutorials/(?P<pk>[0-9]+)$', views.task_detail),
+    #re_path(r'^/tutorials/published$', views.task_published)
+]
