4a349db9dff31a7ad5dc5e92c3b58b4a72d4a3e5
connorswanson1 <89426861+connorswanson1@users.noreply.github.com>
2022-10-03 22:10:38 -0600

    Add files via upload
    Early version of a view tasks page. Not exactly sure how to get task data from the create-task page, so I have just been generating a list of dummy tasks to display on this page with a main method that can be commented out. This page allows for navigation back and forth between a task list and task detail page. Let me know what I can add.

---
+++ gamified_task_tracker/lib/view_tasks.dart
@@ -0,0 +1,105 @@
+import 'package:flutter/material.dart';
+import 'style.dart';
+
+class Task {
+  final String task_name;
+  final String description;
+  final String points;
+
+  Task(this.task_name, this.description, this.points);
+}
+
+/* used for testing - generates a dummy list of tasks */
+void main() {
+  runApp(MaterialApp(
+    title: 'Tasks Page',
+    home: ViewTasksPage(
+      // generate list
+      tasks: List.generate(
+        10,
+        (i) => Task(
+          'Task $i',
+          'Task Description $i',
+          'Points $i',
+        ),
+      ),
+    ),
+  ));
+}
+//*/
+
+// Tasks screen
+class ViewTasksPage extends StatelessWidget {
+  final List<Task> tasks;
+
+  const ViewTasksPage({Key? key, required this.tasks}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: backgroundColor,
+      appBar: AppBar(
+        title: const Text("Tasks",
+            style: TextStyle(
+              color: textColorAgainstPrimary,
+            )),
+        centerTitle: true,
+        leading: GestureDetector(
+            child: const Icon(
+          Icons.arrow_back,
+          color: textColorAgainstPrimary,
+        )),
+        backgroundColor: primaryColor,
+      ),
+      // List builder
+      body: ListView.builder(
+        itemCount: tasks.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text(tasks[index].task_name),
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => ViewTasksDetail(task: tasks[index]),
+                ),
+              );
+            },
+          );
+        },
+      ),
+    );
+  }
+}
+
+// detail screen
+class ViewTasksDetail extends StatelessWidget {
+  final Task task;
+  const ViewTasksDetail({Key? key, required this.task}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("${task.task_name}: ${task.points}",
+              style: const TextStyle(
+                color: textColorAgainstPrimary,
+              )),
+          leading: GestureDetector(
+              child: const Icon(
+                Icons.arrow_back,
+                color: textColorAgainstPrimary,
+              ),
+              onTap: () {
+                Navigator.pop(context);
+              }),
+          backgroundColor: primaryColor,
+        ),
+        body: SingleChildScrollView(
+            scrollDirection: Axis.vertical,
+            child: Container(
+              padding: const EdgeInsets.all(edgeInsets),
+              child: Text(task.description),
+            )));
+  }
+}
