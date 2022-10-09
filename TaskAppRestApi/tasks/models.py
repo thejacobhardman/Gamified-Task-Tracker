0fe762df9398490539d8e980d9f50ad6076fe157
prilika1 <53357004+prilika1@users.noreply.github.com>
2022-10-08 18:29:47 -0600

    editable false

--- TaskAppRestApi/tasks/models.py
+++ TaskAppRestApi/tasks/models.py
@@ -8,7 +8,7 @@ import uuid
 class Team(models.Model):
     team_name = models.CharField(max_length=70, blank=False, default='')
     team_code = models.CharField(
-        blank=False, unique=True, max_length=6, editable=False)
+        blank=False, unique=True, max_length=6)
 
 
 class User(models.Model):
