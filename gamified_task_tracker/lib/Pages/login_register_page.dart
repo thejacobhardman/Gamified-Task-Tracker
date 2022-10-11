f9f2cd7d79a6fb0372ce7608dff75734b3241c90
Mat <davi8791@bears.unco.edu>
2022-10-10 20:55:11 -0600

    Added styling to login/register page

--- gamified_task_tracker/lib/Pages/login_register_page.dart
+++ gamified_task_tracker/lib/Pages/login_register_page.dart
@@ -1,17 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:gamified_task_tracker/widgets/ttscaffold.dart';
+import '../style.dart';
+import '../widgets/ttform.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:gamified_task_tracker/Models/users.dart';
 import 'package:gamified_task_tracker/RemoteAccess.dart';
 import '../auth.dart';
 
 class LoginPage extends StatefulWidget {
+
   const LoginPage({Key? key}) : super(key: key);
 
   @override
   State<LoginPage> createState() => _LoginPageState();
+  
 }
 
 class _LoginPageState extends State<LoginPage> {
+
   String? errorMessage = '';
   bool isLogin = true;
   RemoteAccess access = RemoteAccess();
@@ -66,80 +72,68 @@ class _LoginPageState extends State<LoginPage> {
     }
   }
 
-  Widget _title() {
-    return const Text('Gamified Task Tracker');
-  }
-
-  Widget _entryField(
-    String title,
-    TextEditingController controller,
-  ) {
-    return TextField(
-        controller: controller,
-        decoration: InputDecoration(
-          labelText: title,
-        ));
-  }
-
-  Widget _usernameField(
-      String title,
-      TextEditingController controller,
-      ) {
-    return Visibility(
-        visible: !isLogin,
-        child: TextField(
-        controller: controller,
-        decoration: InputDecoration(
-          labelText: title,
-        )));
-
-
-  }
-
-  Widget _errorMessage() {
-    return Text(errorMessage == '' ? '' : 'Hmm ? $errorMessage');
-  }
-
-  Widget _submitButton() {
-    return ElevatedButton(
-      onPressed:
-          isLogin ? signInWithEmailAndPassword : createUserWithEmailAndPassword,
-      child: Text(isLogin ? 'Login' : 'Register'),
-    );
-  }
-
-  Widget _loginOrRegisterButton() {
-    return TextButton(
-      onPressed: () {
-        setState(() {
-          isLogin = !isLogin;
-        });
-      },
-      child: Text(isLogin ? 'Register instead' : 'Login instead'),
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: _title(),
+    return TTScaffold(
+      bgColor: backgroundColor,
+      title: '',
+      hasAppBar: false,
+      body: TTForm(
+          children: <Widget>[
+            
+            const TTFormElement(
+              verticalPaddingOnly: true,
+              child: Text(
+                'Gamified Task Tracker',
+                style: TextStyle(
+                  color: textColor, 
+                  fontSize: 30
+                  ),
+                ),
+              ),
+
+            TTTextField(
+              visible: !isLogin,
+              controller: _controllerUsername,
+              labelText: 'Username'
+              ),
+
+            TTTextField(
+              controller: _controllerEmail,
+              labelText: 'Email'
+              ),
+
+            TTTextField(
+              controller: _controllerPassword,
+              obscureText: true,
+              labelText: 'Password'
+              ),
+
+            TTFormElement(
+              child: Text(
+                errorMessage == '' ? '' : 'Hmm ? $errorMessage'
+                ),
+              ),
+            
+            TTButton(
+              text: isLogin ? 'Login' : 'Register',
+              onPressed: isLogin ? signInWithEmailAndPassword : createUserWithEmailAndPassword,
+              ),
+
+            TTButton(
+              text: isLogin ? 'Register instead' : 'Login instead',
+              onPressed: () {
+                  setState(() {
+                    isLogin = !isLogin;
+                  });
+                },
+              ),
+
+          ],
         ),
-        body: Container(
-            height: double.infinity,
-            width: double.infinity,
-            padding: const EdgeInsets.all(20),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                _usernameField('username', _controllerUsername),
-                _entryField('email', _controllerEmail),
-                _entryField('password', _controllerPassword),
-                _errorMessage(),
-                _submitButton(),
-                _loginOrRegisterButton(),
-              ],
-            )));
+      );
   }
 }
+
+
+
