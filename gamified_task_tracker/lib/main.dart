5c6449ad94e8d641c89fd8e90a5c12cbba3a9a99
cox9176 <cox9176@bears.unco.edu>
2022-10-13 03:24:27 -0600

    Finish Config of Firebase Auth for iOS

--- gamified_task_tracker/lib/main.dart
+++ gamified_task_tracker/lib/main.dart
@@ -2,9 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:gamified_task_tracker/Views/widget_tree.dart';
 import 'package:firebase_core/firebase_core.dart';
 
+import 'firebase_options.dart';
+
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+      name: 'gamified task tracker',
+      options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
